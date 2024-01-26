a7df3b1ff5bb87e05c90f55d0a0c2028a81151fa
Ibrohim Mubarok <befousmain@gmail.com>
2024-01-26 09:52:34 +0700

    asdasd

--- js/controller/registrasi.js
+++ js/controller/registrasi.js
@@ -1,27 +1,35 @@
 import { postHeader } from "./api.js";
 import { getValue } from "./element.js";
-import { TambahFormDosenAPI } from "./gudangAPI.js";
+import { RegistrasiAPI } from "./gudangAPI.js";
 import { getCookie } from "./cookies.js";
 
 
-export default function createDosen(){
+export default function Registrasi(){
     // tokenstring = getCookie("token")
     let tokenkey = "token";
     let tokenvalue = getCookie("token");
+    let inirole = getValue("role_akun_admin");
 
-
-    let datainjson = {
-        "username": getValue("username_akun_admin"),
-        "password": getValue("password_akun_admin"),
-        "role": getValue("role_akun_admin"),
-        "nik": getValue("nik_akun_admin")
+    if (inirole === "admin") {
+        let datainjson = {
+            "username": getValue("username_akun_admin"),
+            "password": getValue("password_akun_admin"),
+            "role": getValue("role_akun_admin")
+        };
+        postHeader(RegistrasiAPI,datainjson,tokenkey,tokenvalue,responseData);
     };
-
-    
-    postHeader(TambahFormDosenAPI,datainjson,tokenkey,tokenvalue,responseData);
+    if (inirole === "dosen") {
+        let datainjson = {
+            "username": getValue("username_akun_admin"),
+            "password": getValue("password_akun_admin"),
+            "role": getValue("role_akun_admin"),
+            "nik": getValue("nik_akun_admin")
+        };
+        postHeader(RegistrasiAPI,datainjson,tokenkey,tokenvalue,responseData);
+    };
 }
 
 function responseData(result) {
     alert(result.message);
-    window.location.href = "./detaildosen.html";
+    window.location.href = "./semuaakun.html";
 }
